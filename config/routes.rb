f8da4085f25b154cd8e91382ccfcd2f20dec9337
Geoffrey <mesniergeoffrey@gmail.com>
2021-03-10 13:55:16 +0100

    Css button design

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root to: 'pages#home'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+  # Route for design css
 
   resources :games, only: [:create, :show] do
     resources :players, only: [:create]
@@ -19,5 +20,7 @@ Rails.application.routes.draw do
     #   get :top
     # end
   end
+
+  get "components", to: 'pages#components'
 
 end
