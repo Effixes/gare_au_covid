692b3d1b645423bbe7cfef4bf33896b8ea30df0b
Justine Saupin <sau.justine@gmail.com>
2021-03-11 15:33:04 +0100

    end-turn method

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,9 +1,8 @@
 class GamesController < ApplicationController
-  before_action :set_game, only: [:show, :draw, :start]
+  before_action :set_game, only: [:show, :draw, :start, :end_turn]
 
   def show
-
-    @curent_status = 
+    @curent_status =
       if @game.status == 'waiting' && player_has_not_joined_game?
         @player = Player.new
         'player_invited'
@@ -13,47 +12,48 @@ class GamesController < ApplicationController
         'on_going'
       end
 
-      render @curent_status
-    end
+    render @curent_status
+  end
 
-    def create
-      @game = Game.new(game_params)
-      @game.status = "waiting"
-      @game.host = @game.players.first
-      @game.save!
-      set_current_player(@game.players.first)
-      redirect_to game_path(@game)
-    end
-  
-    def start
-      if current_player == @game.host
-        StartGame.new(@game).call
-      else
-        flash[:alert] = "En attente de la création de la partie"
-      end
-      redirect_to game_path(@game)
-    end
-    
-    def play_card(card_code)
-      PlayCard.new(@game, card_code).call
-    end
-  
-    def draw
-      drawed_card_code = DrawCard.new(@game).call
-      redirect_to game_path(@game, drawed_card_code: drawed_card_code)
-    end
+  def create
+    @game = Game.new(game_params)
+    @game.status = "waiting"
+    @game.host = @game.players.first
+    @game.save!
+    set_current_player(@game.players.first)
+    redirect_to game_path(@game)
+  end
 
-    private
-
-    def player_has_not_joined_game?
-      current_player.nil? || @game.player_ids.include?(current_player.id) == false
+  def start
+    if current_player == @game.host
+      StartGame.new(@game).call
+    else
+      flash[:alert] = "En attente de la création de la partie"
     end
+    redirect_to game_path(@game)
+  end
 
-    def set_game
-      @game = Game.find(params[:id])
-    end
+  def draw
+    drawed_card_code = DrawCard.new(@game).call
+    redirect_to game_path(@game, drawed_card_code: drawed_card_code)
+  end
 
-    def game_params
-      params.require(:game).permit(:players_count, players_attributes: [:name, :avatar])
-    end
+  def end_turn
+    EndTurn.new(@game).call
+    redirect_to game_path(@game)
+  end
+
+  private
+
+  def player_has_not_joined_game?
+    current_player.nil? || @game.player_ids.include?(current_player.id) == false
+  end
+
+  def set_game
+    @game = Game.find(params[:id])
+  end
+
+  def game_params
+    params.require(:game).permit(:players_count, players_attributes: [:name, :avatar])
+  end
 end
