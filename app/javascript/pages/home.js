bc91137272eb66a979531e76dacbb2ea817c6260
Justine Saupin <sau.justine@gmail.com>
2021-03-12 15:53:27 +0100

    avatars + endgame

--- app/javascript/pages/home.js
+++ app/javascript/pages/home.js
@@ -4,17 +4,21 @@ const updateHomeCounter = () => {
     let plus = document.getElementById("plus");
     let minus = document.getElementById("minus");
 
-    plus.addEventListener("click", function(){ 
-        count++;
-        countEl.value = count;
-    }); 
+    if (plus) {
+      plus.addEventListener("click", function(){
+          count++;
+          countEl.value = count;
+      });
+    }
 
-    minus.addEventListener("click", function(){ 
-        if (count > 1) {
-            count--;
-            countEl.value = count;
-        }  
-    }); 
+    if (minus) {
+      minus.addEventListener("click", function(){
+          if (count > 1) {
+              count--;
+              countEl.value = count;
+          }
+      });
+    }
 }
 
 export { updateHomeCounter };
