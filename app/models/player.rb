fe1221bc09760d95e7a17bfef3280b707c8a7c72
allan boodhun <allan.boodhun@gmail.com>
2021-03-12 16:47:01 +0100

    fix indian

--- app/models/player.rb
+++ app/models/player.rb
@@ -1,5 +1,5 @@
 class Player < ApplicationRecord
-  AVATARS = ["nun.png", "batman.png", "harley.png", "indian_us", "jason.png", "sexy_man.png", "sheep.png", "sloth.png", "white.png", "wonderw.png"]
+  AVATARS = ["nun.png", "batman.png", "harley.png", "indian_us.png", "jason.png", "sexy_man.png", "sheep.png", "sloth.png", "white.png", "wonderw.png"]
 
   belongs_to :game
 end
