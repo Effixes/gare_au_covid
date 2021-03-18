67b0a56fabee16b7b9b9633f1607792e8275970c
Geoffrey MESNIER <44602983+GeoffreyMesnier@users.noreply.github.com>
2021-03-18 10:28:44 +0100

    board, helpar card

--- app/helpers/card_helper.rb
+++ app/helpers/card_helper.rb
@@ -1,6 +1,6 @@
 module CardHelper
-  def playable?(card_code)
-    return false if current_player.draw_card_count == 0
+  def playable?(card_code, player)
+    return false if player.draw_card_count == 0
 
     # Recupere le jeu en cours
 
@@ -23,12 +23,12 @@ module CardHelper
     # Lock down
     when "lock_down"
       # Carte jouable si nomnbre de carte a piocher >= 1
-      current_player.draw_card_count >= 1
+      player.draw_card_count >= 1
 
     # Pairs
     when "pair_pangolins", "pair_teletravail", "pair_villageoise", "pair_masques", "pair_couvre_feu"
       # Init compteur
-      count_pair = current_player.cards.count { |card| card == card_code }
+      count_pair = player.cards.count { |card| card == card_code }
       count_pair >= 2
     else
       true
